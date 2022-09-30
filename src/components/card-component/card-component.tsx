1a4bb515e3845dcabf7dbe721ae74a66b9f002e8
Matt Skelton <matt.skelton@sds.co.uk>
2022-09-30 15:24:28 +0100

    Added basic card component

---
+++ src/components/card-component/card-component.tsx
@@ -0,0 +1,26 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'card-component',
+  styleUrl: 'card-component.css',
+  shadow: true,
+})
+export class CardComponent {
+  @Prop() cardTitle: string | null = null;
+  @Prop() cardClass: string;
+  @Prop() cardHeaderClass: string;
+
+  render() {
+    return (
+      <div class="card {this.cardClass}">
+        if (this.cardTitle)
+        {
+          <div class="card__header {this.cardHeaderClass}">
+            <h3>{this.cardTitle}</h3>
+          </div>
+        }
+        <slot></slot>
+      </div>
+    );
+  }
+}
