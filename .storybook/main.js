c23a559d07fd2e3e56621eabd6668b7c7c26cfea
Ben Singer <ben.singer@sds.co.uk>
2022-10-04 13:57:44 +0100

    adding additional helper deps for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,9 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-notes",
+    "@storybook/addon-viewport"
   ],
   "framework": "@storybook/html",
   "core": {
